7171d9e756c25cfac1c7919c70d35f53b351e807
ChristopherThorne714 <christhorne714@gmail.com>
2025-09-10 15:21:54 -0400

    fixed some minor bugs

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { loggerMiddleware } = require('./util/logger');
-const app = express();
 
 const userRoutes = require('./routes/api/userRoutes');
-const ticketRoutes = require('./routes/api/ticketRoutes');
+// const ticketRoutes = require('./routes/api/ticketRoutes');
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(loggerMiddleware);
 
-app.use('/api/users/', userRoutes);
-app.use('/api/tickets/', ticketRoutes);
+app.use('/api/users', userRoutes);
+// app.use('/api/tickets', ticketRoutes);
 
-module.expots = app;
+module.exports = app;
--- backend/routes/api/userRoutes.js
+++ backend/routes/api/userRoutes.js
@@ -4,3 +4,6 @@ const router = express.Router();
 const userController = require('../../controllers/userController');
 
 router.post('/login', userController.Login);
+
+
+module.exports = router;
