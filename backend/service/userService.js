ea53fecd2b4508dfc5570eeed876bdb595d256d0
ChristopherThorne714 <christhorne714@gmail.com>
2025-09-11 16:05:14 -0400

    .

--- backend/service/userService.js
+++ backend/service/userService.js
@@ -6,6 +6,7 @@ const { logger } = require('../util/logger');
 async function postUser(user) {
     const saltRounds = 10; 
     const role = !user.role ? "employee" : user.role;
+    // next line contains a bug
     if (validateUser(user) && await !userDAO.findUserByUsername(user.username)) {
         const password = await bcrypt.hash(user.password, saltRounds);
         const data = await userDAO.createUser({
